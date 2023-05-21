4346aaeb095f3866f96519e87b014e5e0a4097cf
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-05-21 11:16:59 -0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import React from 'react'
 import './App.css'
+import { getMonth } from './util'
+import CalendarHeader from './components/CalendarHeader'
+import Sidebar from './components/Sidebar'
+import Month from './components/Month'
 
 function App() {
 
+  console.table(getMonth(3))
+
   return (
-    <>
-      
-    </>
+    <React.Fragment>
+      <div className='h-screen flex flex-columns'>
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month />
+        </div>
+      </div>
+    </React.Fragment>
   )
 }
 
